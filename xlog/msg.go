d78c1217fb5098f573577d91d0a4cad7da17cb0a
abcdlsj <lisongjianshuai@gmail.com>
2023-05-12 09:56:49 +0800

    add simple xlog impl

---
+++ xlog/msg.go
@@ -0,0 +1,21 @@
+package xlog
+
+type Msg struct {
+	msg    string
+	fields map[string]interface{}
+}
+
+var nilMsg = Msg{
+	msg:    "",
+	fields: nil,
+}
+
+func (m Msg) String() string {
+	s := m.msg
+
+	for k, v := range m.fields {
+		s += " " + k + "=" + v.(string)
+	}
+
+	return s
+}
